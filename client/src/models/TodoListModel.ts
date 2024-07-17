c9ca8ef529873d508e007e6994d9bcebddea5e89
Ashish Sharma <ashishsharma903461@gmail.com>
2024-07-17 12:37:05 +0530

    status change bug fix

--- client/src/models/TodoListModel.ts
+++ client/src/models/TodoListModel.ts
@@ -1,8 +1,9 @@
-export type TodoList={
-    id:string,
-    title:string,
-    description:string,
-    date:string,
-    priority:string,
-    isCompleted:boolean
-}
+export type TodoList = {
+  id: string;
+  title: string;
+  description: string;
+  date: string;
+  priority: string;
+  isCompleted: boolean;
+  history: Array<{ date: string; action: string }>;
+};
